3c0b0428d4b0e567d8c39c7ad3907f0fccb27a77
aman <atmanig0@gmail.com>
2026-04-26 13:40:19 +0300

    solving the problem and setting the leftover the last 2 CRUD

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const http = require('http');
-const  movies = require('./data/movie.json');
+const  path = './data/movie.json';
 const fs = require('fs');
 
 const server = http.createServer((req,res) => {
@@ -8,15 +8,17 @@ const server = http.createServer((req,res) => {
     
     res.writeHead(200,{'content-type':'application/json'});
     res.end(JSON.stringify(movie));
+
   } else if (req.url.startsWith('/api/movie/') && req.method === 'GET') {
-    const movieId = req.url.split('/')[3]
+    const id = parseInt(req.url.split('/')[3]);
+    const movies = JSON.parse(fs.readFileSync(path));
     const movie = movie.find(m => m.id === id);
 
     if (movie) {
       res.writeHead(200, {'content-type':'application/json'});
       res.end(JSON.stringify(movie));
     } else {
-      res.writeHead(404, {'content-type':'text/plain'});
+      res.writeHead(404);
       res.end('not included');
     }
 
@@ -29,14 +31,60 @@ const server = http.createServer((req,res) => {
     req.on('end',async () => {
       const newmovie = JSON.parse(body);
       const movies = await JSON.parse(fs.readFileSync(filePath));
-      movies.push(newMovie);
+      newmovie.id = movies.length + 1;
+      movies.push(newmovie);
 
-      fs.writeFileSync(filePath, JSON.stringify(movies, null, 2));
+      fs.writeFileSync(filePath, JSON.stringify(movies));
 
       res.writeHead(201, { 'Content-Type': 'application/json' });
       res.end(JSON.stringify(newMovie)); 
 
     });
+
   }
- 
+ else if (req.url.startsWith('/api/movies/') && req.method === 'PUT') {
+    const id = parseInt(req.url.split('/')[3]);
+    let body = '';
+
+    req.on('data', chunk => {
+      body += chunk.toString();
+    });
+
+    req.on('end', () => {
+      const updatedData = JSON.parse(body);
+      let movies = JSON.parse(fs.readFileSync(path));
+
+      movies = movies.map(m =>
+        m.id === id ? { ...m, ...updatedData } : m
+      );
+
+      fs.writeFileSync(path, JSON.stringify(movies));
+
+      res.writeHead(200);
+      res.end('Movie updated');
+    });
+  
+  }
+
+ else if (req.url.startsWith('/api/movies/') && req.method === 'DELETE') {
+    const id = parseInt(req.url.split('/')[3]);
+    let movies = JSON.parse(fs.readFileSync(path));
+
+    movies = movies.filter(m => m.id !== id);
+
+    fs.writeFileSync(path, JSON.stringify(movies));
+
+    res.writeHead(200);
+    res.end('Movie deleted');
+  }
+
+  else {
+    res.writeHead(404);
+    res.end('Route not found');
+  }
+
+});
+
+server.listen(4000, () => {
+  console.log('server running on port 4000');
 });
